49c9e138cb44dfeb10cd8491c0eafb1af1ba1e72
12BiancaMunoz <paulette-bms@hotmail.com>
2020-07-09 13:25:33 -0500

    tare2

--- app/src/main/java/com/example/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -5,33 +5,97 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mTrueButton;
-    private Button mFalseButton;
+    private Button btnVerdadero;
+    private Button btnFalso;
+    private Button btnsiguiente;
+    private Button btnanterior;
+
+    private TextView txtPregunta;
+
+    private Pregunta[] preguntas={
+            new Pregunta(R.string.pregunta_1, true),
+            new Pregunta(R.string.pregunta_2, true),
+            new Pregunta(R.string.pregunta_3, true),
+            new Pregunta(R.string.pregunta_4, true),
+           new Pregunta(R.string.pregunta_5, false),
+
+
+    };
+
+    private Pregunta[] mPreguntas= new Pregunta[5];
+    int actual =0;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Enables Always on
-        mTrueButton=(Button)findViewById(R.id.btn_verdadero);
-        mTrueButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"Correcto",Toast.LENGTH_SHORT).show();
+
+        btnVerdadero=(Button)findViewById(R.id.btn_verdadero);
+        btnFalso=(Button)findViewById(R.id.btn_falso);
+        btnanterior=findViewById(R.id.btn_anterior);
+        btnsiguiente=findViewById(R.id.btn_siguiente);
+        txtPregunta=findViewById(R.id.txt_preguntas);
+
+        //Pregunta p0= preguntas[0];
+        //txtPregunta.setText(p0.getTextoId());
+
+        setText();
+
+        btnVerdadero.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View v) {
+                /*boolean respuesta= preguntas[actual].isRespuesta();
+                if(respuesta){
+                    Toast.makeText(MainActivity.this,"Correcto",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this,"Incorrecto",Toast.LENGTH_SHORT).show();
+                }*/
+                verificarRespuesta(true);
             }
         });
-        mFalseButton=(Button)findViewById(R.id.btn_falso);
-        mFalseButton.setOnClickListener(new View.OnClickListener() {
+
+        btnFalso.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View v) {
+                verificarRespuesta(false);
+            }
+        });
+
+
+        btnsiguiente.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"Incorrecto",Toast.LENGTH_SHORT).show();
+            public void onClick(final View view){
+                actual=(actual+1)% preguntas.length;
+                setText();
 
             }
         });
+
+        btnanterior.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(final View view){
+                actual=(actual-1)% preguntas.length;
+                setText();
+
+            }
+        });
+
+
+    }
+    void setText(){
+        txtPregunta.setText(preguntas[actual].getTextoId());
+    }
+    void verificarRespuesta(boolean result){
+        boolean respuesta= preguntas[actual].isRespuesta();
+
+        if(respuesta==result){
+            Toast.makeText(MainActivity.this,"Correcto",Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(MainActivity.this,"Incorrecto",Toast.LENGTH_SHORT).show();
+        }
     }
 }
